7935a142a0259e423a4c9c5c1d3a11ec5c95612f
Hlaluko <ngobenih45@gmail.com>
2026-08-22 11:12:51 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const items=document.querySelectorAll(".reveal");
-window.addEventListener("scroll",()=>{
-items.forEach(i=>{
-let top=i.getBoundingClientRect().top;
-if(top<window.innerHeight-80)i.style.opacity=1;
-});
-});
